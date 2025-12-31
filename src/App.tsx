d90f6a860ff7b4dc5f8daa97ddd821de2c7ea094
Pathi-02 <pathimilan03@gmail.com>
2026-01-01 03:40:16 +0530

    now resource ok

--- src/App.tsx
+++ src/App.tsx
@@ -87,28 +87,115 @@ const App = () => {
               <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <Routes>
                   {/* Public routes with Layout */}
-                  <Route path="/" element={<Layout><HomePage /></Layout>} />
-                  <Route path="/" element={<Layout><HomePage /></Layout>} />
-                  <Route path="/about" element={<Layout><AboutPage /></Layout>} />
-                  <Route path="/under-construction" element={<Layout><UnderConstructionPage /></Layout>} />
-                  <Route path="/committee" element={<Layout><CommitteePage /></Layout>} />
-                  <Route path="/exam" element={<Layout><ExamPage /></Layout>} />
-                  <Route path="/seminar" element={<Layout><SeminarPage /></Layout>} />
-                  <Route path="/events" element={<Layout><EventsPage /></Layout>} />
-                  <Route path="/events/:id" element={<Layout><EventDetailsPage /></Layout>} />
-                  <Route path="/donate" element={<Layout><DonatePage /></Layout>} />
-                  <Route path="/login" element={<Layout><LoginPage /></Layout>} />
-                  <Route path="/signup" element={<Layout><SignupPortalPage /></Layout>} />
-                  <Route path="/register" element={<Navigate to="/signup" replace />} />
+                  <Route
+                    path="/"
+                    element={
+                      <Layout>
+                        <HomePage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/about"
+                    element={
+                      <Layout>
+                        <AboutPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/under-construction"
+                    element={
+                      <Layout>
+                        <UnderConstructionPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/committee"
+                    element={
+                      <Layout>
+                        <CommitteePage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/exam"
+                    element={
+                      <Layout>
+                        <ExamPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/resources"
+                    element={
+                      <Layout>
+                        <ResourcesPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/seminar"
+                    element={
+                      <Layout>
+                        <SeminarPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/events"
+                    element={
+                      <Layout>
+                        <EventsPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/events/:id"
+                    element={
+                      <Layout>
+                        <EventDetailsPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/donate"
+                    element={
+                      <Layout>
+                        <DonatePage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/login"
+                    element={
+                      <Layout>
+                        <LoginPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/signup"
+                    element={
+                      <Layout>
+                        <SignupPortalPage />
+                      </Layout>
+                    }
+                  />
+                  <Route
+                    path="/register"
+                    element={<Navigate to="/signup" replace />}
+                  />
 
                   {/* ✅ Recommended clean profile route */}
 
                   <Route
                     path="/profile"
                     element={
-                      <AdminAuthProvider>
-                        <Layout><ProfilePage /></Layout>
-                      </AdminAuthProvider>
+                      <Layout>
+                        <ProfilePage />
+                      </Layout>
                     }
                   />
                   {/* Feedback form moved to footer; no dedicated page */}
@@ -148,7 +235,10 @@ const App = () => {
 
                     {/* ✅ ADDED: Results page route */}
                     <Route path="results" element={<AdminResultsPage />} />
-                    <Route path="designations" element={<AdminDesignationsPage />} />
+                    <Route
+                      path="designations"
+                      element={<AdminDesignationsPage />}
+                    />
                     <Route path="site-mode" element={<AdminSiteModePage />} />
 
                     <Route path="events" element={<AdminEventsPage />} />
